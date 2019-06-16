b2a4a0561bcd13b99d5de045d8da2973149ca172
Mszyca Stefan <arekkonior@gmail.com>
2019-06-16 16:39:05 +0300

    simplify version

--- main.cpp
+++ main.cpp
@@ -1,115 +1,94 @@
 #include <iostream>
 #include <thread>
 #include <mutex>
-#include <cstdlib>
 #include <string>
 #include <queue>
 #include <vector>
 #include <functional>
+#include <condition_variable>
+#include <algorithm>
 
 static unsigned int hardware_threads = std::thread::hardware_concurrency();
-
-
-std::mutex global_mutex;
-
-struct Job
-{
-    Job(const std::string& name) : name_(name){};
-    std::string name_;
-    void process()
-    {
-        std::cout << name_ << " " << std::this_thread::get_id() << " start!\n";
-        std::this_thread::sleep_for(std::chrono::seconds(4));
-        std::cout << name_ << " " << std::this_thread::get_id() << " end!\n";
-    }
-};
-
-void dummy()
-{
-    {
-        std::lock_guard<std::mutex> gl(global_mutex);
-        std::cout << "Hej " << std::this_thread::get_id() << '\n';
-    }
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-}
-
+std::mutex ioMutex_;
 
 class ThreadPool
 {
     public:
-        ThreadPool() : workers_(workersNum_)
+        ThreadPool(std::size_t numOfWorkers) : 
+            numOfWorkers_(numOfWorkers),
+            workers_(numOfWorkers_)
         {
-            for(std::size_t i = 0; i < workersNum_; i++)
+            for(std::size_t i = 0; i < numOfWorkers_; ++i)
             {
-                workers_.push_back(std::thread(&ThreadPool::loop_func, this));
-                // workers_.push_back(std::thread(dummy));
+               workers_.push_back(std::thread(&ThreadPool::loop, this, 2));
             }
         }
+
         ~ThreadPool()
         {
-            // for(std::size_t i = 0; i < workersNum_; i++)
-            // {
-            //     workers_[i].join();
-            // }
             std::for_each(workers_.begin(), workers_.end(), std::mem_fn(&std::thread::join));
         }
-        void add_job(std::function<void()> job)
+
+        void pushVal(int val)
         {
-            {
-                std::unique_lock<std::mutex> lock(queue_mutex_);
-                queue_jobs_.push(job);
-            }
-            queue_cond_.notify_one();
+            std::unique_lock<std::mutex> qlock(queueMutex_);
+            queue_.push(val);
+            queue_empty_.notify_one();
         }
+
     private:
-        void loop_func()
+        void loop(uint16_t worktime)
         {
             while(true)
             {
-                std::function<void()> job;
+                int val;
+
                 {
+                    std::unique_lock<std::mutex> qlock(queueMutex_);
+                    queue_empty_.wait(qlock, [this]
                     {
-                        std::unique_lock<std::mutex> iolock(global_mutex);
-                        std::cout << "Thread: " << std::this_thread::get_id() <<
-                        " waiting for non-empty queue: \n";
-                    }
-
-                    std::unique_lock<std::mutex> lock(queue_mutex_);
-                    queue_cond_.wait(lock, [this]{return !queue_jobs_.empty();});
-
-                    {
-                        std::unique_lock<std::mutex> iolock(global_mutex);
-                        std::cout << "Job on queue " << std::this_thread::get_id() << " took it " << '\n';
-                    }
-
-                    job = queue_jobs_.front();
-                    queue_jobs_.pop();
-                    
+                        std::cout << "Notified!" << std::endl;
+                        return !queue_.empty();
+                    });
+                    val = queue_.front();
+                    queue_.pop();
                 }
-                job();
+
+                {
+                    std::unique_lock<std::mutex> iolock(ioMutex_);
+                    std::cout << "Working with value " << val << " for " << worktime << " seconds in thread: " << std::this_thread::get_id() << std::endl;
+                }
                 
+                std::this_thread::sleep_for(std::chrono::seconds(worktime));
+                
+                {
+                    std::unique_lock<std::mutex> iolock(ioMutex_);
+                    std::cout << "Thread " << std::this_thread::get_id() << " is done" << std::endl;
+                }
             }
         }
-        std::size_t workersNum_ = 6;
+
+        std::size_t numOfWorkers_;
         std::vector<std::thread> workers_;
-        std::mutex queue_mutex_;
-        std::condition_variable queue_cond_;
-        std::queue<std::function<void()>> queue_jobs_;
+        
+        std::mutex queueMutex_;
+
+        std::queue<int> queue_;
+        std::condition_variable queue_empty_;
 };
 
 int main()
 {
-    ThreadPool tp;
-
-    Job job1("first");
-    Job job2("second");
-
-    tp.add_job(std::bind(&Job::process, &job1));
-    tp.add_job(std::bind(&Job::process, &job2));
-
+    {
+        std::unique_lock<std::mutex> iolock(ioMutex_);
+        std::cout << "Hello from main! Threads: " << hardware_threads << '\n';
+    }
+    ThreadPool tp(hardware_threads-1);
     while(true)
-    {}
-
-    
+    {
+        int val;
+        std::cin >> val;
+        tp.pushVal(val);
+    }
     return 0;
 }
